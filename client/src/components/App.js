32f282c08d170051045b752a3683efb4345714d0
Anupama94 <anupama.arukgoda@gmail.com>
2019-03-28 11:06:49 +0530

    creating an Error Page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import Login from "./Login/Login";
 import OrderList from "./OrderList/OrderList";
 import OrderDetailsView from "./OrderDetails/OrderDetailsView";
 import ProtectedRoute from "../authentication/protectedRoutes";
+import Page404 from './Page404';
 
 class App extends React.Component {
     render() {
@@ -15,6 +16,7 @@ class App extends React.Component {
                     <Route path="/" component={Login} exact/>
                     <ProtectedRoute path="/OrderList" component={OrderList} exact/>
                     <ProtectedRoute path="/OrderDetails/:id" component={OrderDetailsView} exact/>
+                    <Route path="" component={Page404} />
                 </Switch>
             </BrowserRouter>
         );
